df11cf8c804139351ec4117b12fa0aaf864fe9f6
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2024-06-14 14:51:07 +0530

    Refactored. Organized test cases into separate context for add method

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,17 +3,20 @@ require 'string_calculator'
 
 describe StringCalculator do
   describe "add" do
-    it "return 0 if input is an empty string" do
-      expect(StringCalculator.add("")).to eql(0)
+    context "without delimiter" do
+      it "return 0 if input is an empty string" do
+        expect(StringCalculator.add("")).to eql(0)
+      end
+
+      it "return input string as number when it is not empty" do
+        expect(StringCalculator.add("1")).to eql(1)
+      end
     end
 
-    it "return input string as number if there is no delimiter in the input string" do
-      expect(StringCalculator.add("1")).to eql(1)
+    context "with delimiter" do
+      it "returns sum of numbers in a comma-separated string" do
+        expect(StringCalculator.add("1,2")).to eql(3)
+      end
     end
-
-    it "returns sum of numbers in a comma-separated string" do
-      expect(StringCalculator.add("1,2")).to eql(3)
-    end
-
   end
 end
