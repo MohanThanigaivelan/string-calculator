749266b575f334ee38c57e70faadf97d9d8aea02
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2024-06-14 14:43:53 +0530

    Add Feature #3 . Add numbers in a comma separated string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 module StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.to_i
+
+    numbers.split(',').map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,10 @@ describe StringCalculator do
     it "return input string as number if there is no delimiter in the input string" do
       expect(StringCalculator.add("1")).to eql(1)
     end
+
+    it "returns sum of numbers in a comma-separated string" do
+      expect(StringCalculator.add("1,2")).to eql(3)
+    end
+
   end
 end
