fb097cd9480bd70774220b1cfc27a0acaa048036
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2024-06-14 23:42:49 +0530

    Refactored add method. Ignore custom delimiter and consider only number portion while adding

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,18 +1,18 @@
 module StringCalculator
-  def self.add(numbers)
-    return 0 if numbers.empty?
+  def self.add(input_string)
+    return 0 if input_string.empty?
 
-    delimiters = get_delimiter(numbers)
+    delimiters, numbers = get_delimiter(input_string)
     numbers.split(delimiters).map(&:to_i).sum
   end
 
   private
-  def self.get_delimiter(numbers)
-    if numbers.start_with?("//")
-      custom_delimiter = numbers.split("\n").first[2..-1]
-      /[\n,#{Regexp.escape(custom_delimiter)}]/
+  def self.get_delimiter(input_string)
+    if input_string.start_with?("//")
+      custom_delimiter, numbers = input_string[2..-1].split("\n", 2)
+      [/[\n,#{Regexp.escape(custom_delimiter)}]/ , numbers]
     else
-      /[\n,]/
+      [ /[\n,]/ , input_string]
     end
 
   end
