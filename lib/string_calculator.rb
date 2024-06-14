768aae890d1bbf7a8ae4df685439e340a851db2b
Mohan Thanigaivelan <mohanthanigaivelan@Mohans-MacBook-Pro.local>
2024-06-14 14:31:56 +0530

    Add Feature #2 . Return input string as number when no demiliter present

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 module StringCalculator
   def self.add(numbers)
-    0
+    return 0 if numbers.empty?
+    numbers.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,5 +6,9 @@ describe StringCalculator do
     it "return 0 if input is an empty string" do
       expect(StringCalculator.add("")).to eql(0)
     end
+
+    it "return input string as number if there is no delimiter in the input string" do
+      expect(StringCalculator.add("1")).to eql(1)
+    end
   end
 end
